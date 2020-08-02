ebc64c9956cbd96fe72323d4568a1bb79ce03585
lhh2001 <lihenghao2001@163.com>
2020-08-02 23:48:41 +0800

    使用新的类库(Sequence1)，解决BUG: 胜利后Animation未析构造成的内存泄漏

--- boxman/boxman.cpp
+++ boxman/boxman.cpp
@@ -8,6 +8,7 @@ using namespace GameLib;
 
 const int INPUT_TYPE_SIZE = 5; //输入按键情况数量
 const int INPUT_Q = 4;         //按键Q对应的下标
+const int FPS = 60;            //帧率
 
 char inputTable[5] = {
     'a', 's', 'w', 'd', 'q'
@@ -15,16 +16,12 @@ char inputTable[5] = {
 
 const int shiftX[4] = { 0, -1, 0, 1 };
 const int shiftY[4] = { -1, 0, 1, 0 };
-const int fpsRateSize = 10; //统计帧率所用的连续帧数
 
 std::vector<std::vector<char>> map;   //存储原始地图, 不包含人和箱子
 std::vector<std::vector<char>> image; //上一帧的游戏画面
 
 bool isInit;
 bool playerWantToQuit;
-
-unsigned previousTime[fpsRateSize];
-unsigned frameRate;
 
 Image* gameImage;
 GamePlay* myGame;
@@ -34,22 +31,10 @@ namespace GameLib
 {
 	void Framework::update()
 	{
-        while (time() - previousTime[fpsRateSize - 1] < 16)
-        {
-            sleep(1);
-        }
-        unsigned currentTime = time();
-        unsigned frameTime = currentTime - previousTime[0];
-        for (int i = 0; i < fpsRateSize - 1; i++)
-        {
-            previousTime[i] = previousTime[i + 1];
-        }
-        previousTime[fpsRateSize - 1] = currentTime;
-        frameRate = 1000 * fpsRateSize / frameTime;
-        cout << frameRate << endl;
-
+        cout << frameRate() << endl;
         if (!isInit)
         {
+            setFrameRate(FPS);
             myAnimation = nullptr;
             gameImage = new Image("nimotsuKunImage2.dds");
             myGame = new GamePlay("3");
@@ -61,11 +46,18 @@ namespace GameLib
             {
                 if (!myAnimation->draw())
                 {
-                    delete myAnimation;
-                    myAnimation = nullptr;
+                    SAFE_DELETE(myAnimation);
                 }
                 return;
             }
+
+            if (myGame->check())
+            {
+                playerWantToQuit = true;
+                GameLib::cout << "恭喜! 您共用"
+                    << myGame->getStep() << "步通关!" << GameLib::endl;
+            }
+
             Framework f = Framework::instance();
             for (int i = 0; i < INPUT_TYPE_SIZE; i++)
             {
@@ -82,23 +74,19 @@ namespace GameLib
             }
         }
 
-        if (myGame->check())
-        {
-            playerWantToQuit = true;
-            GameLib::cout << "恭喜! 您共用" 
-                << myGame->getStep() << "步通关!" << GameLib::endl;
-        }
-
         if (playerWantToQuit)
         {
             requestEnd();
         }
         if (isEndRequested())
         {
-            delete myGame;
-            delete gameImage;
+            SAFE_DELETE(myGame);
+            SAFE_DELETE(gameImage);
+
+            #ifdef _DEBUG
             map.~vector();
             image.~vector();
+            #endif //不提前析构map和image憨憨VS会误报内存泄漏
         }
 	}
 }
